1c7f40689322aa033de5f057470448a7bb86d563
Cuong <cuongnt04230@gmail.com>
2025-05-18 08:47:14 +0700

    [Learn] Text view and add it into scroll horizontal

--- Day1/Day1/ContentView.swift
+++ Day1/Day1/ContentView.swift
@@ -9,20 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing: 25) {
-            Text("Knock, knock!")
-                .padding()
-                .background(.yellow, in: RoundedRectangle(cornerRadius: 8.0))
-                .shadow(radius: 8.0)
-            Text("Who's there")
-                .padding()
-                .background(.teal, in: RoundedRectangle(cornerRadius: 8.0))
-                .shadow(radius: 8.0)
-            Text("I'm Rhino")
-                .padding()
-                .background(.cyan, in: RoundedRectangle(cornerRadius: 8.0))
-                .shadow(radius: 8.0)
+        VStack {
+            VStack(spacing: 25) {
+                Text("Knock, knock!")
+                    .padding()
+                    .background(.yellow, in: RoundedRectangle(cornerRadius: 8.0))
+                    .shadow(radius: 8.0)
+                Text("Who's there")
+                    .padding()
+                    .background(.teal, in: RoundedRectangle(cornerRadius: 8.0))
+                    .shadow(radius: 8.0)
+                Text("I'm Rhino")
+                    .padding()
+                    .background(.cyan, in: RoundedRectangle(cornerRadius: 8.0))
+                    .shadow(radius: 8.0)
 
+            }
+            .padding()
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 16) {
+                    Text("funny")
+                        .padding()
+                        .background(.pink, in: RoundedRectangle(cornerRadius: 8.0))
+                        .shadow(radius: 8.0)
+                        .fixedSize()
+                    Text("work hard")
+                        .padding()
+                        .background(.purple, in: RoundedRectangle(cornerRadius: 8.0))
+                        .shadow(radius: 8.0)
+                        .fixedSize()
+                    Text("strong")
+                        .padding()
+                        .background(.green, in: RoundedRectangle(cornerRadius: 8.0))
+                        .shadow(radius: 8.0)
+                        .fixedSize()
+                    Text("nomad")
+                        .padding()
+                        .background(.blue, in: RoundedRectangle(cornerRadius: 8.0))
+                        .shadow(radius: 8.0)
+                        .fixedSize()
+                    Text("developer")
+                        .padding()
+                        .background(.orange, in: RoundedRectangle(cornerRadius: 8.0))
+                        .shadow(radius: 8.0)
+                        .fixedSize()
+                    Text("builder")
+                        .padding()
+                        .background(.mint, in: RoundedRectangle(cornerRadius: 8.0))
+                        .shadow(radius: 8.0)
+                        .fixedSize()
+                }
+                .padding()
+            }
         }
         .padding()
     }
